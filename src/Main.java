0e6097840b7779b439dc51c8385e61c30e367c6c
Pavel038 <98365181+Pavel038@users.noreply.github.com>
2022-12-18 18:01:11 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 300; // Начальный счет
-        int b = 11500; // Сумма пополнений
-        int c = 0; // Бонусные рубли
-        int d = 0; // Итоговый счет
+        int starting_score = 300; // Начальный счет
+        int replenishment_amount = 11500; // Сумма пополнений
+        int bonus_rubles = 0; // Бонусные рубли
+        int final_score = 0; // Итоговый счет
 
-        if (b < 1000) {
-            d = b + a;
-            System.out.println("Клиент пополнил счёт на " + b + " рублей - бонусов нет, итоговая сумма на счету клиента - " + d);
+        if (replenishment_amount < 1000) {
+            final_score = replenishment_amount + starting_score;
+            System.out.println("Клиент пополнил счёт на " + replenishment_amount + " рублей - бонусов нет, итоговая сумма на счету клиента - " + final_score);
 
         } else {
-            c = b / 100;
-            d = b + a + c;
-            System.out.println("Клиент пополнил счёт на " + b + " рублей - бонус равен " + c + " рублям ,итоговая сумма на счету клиента - " + d);
+            bonus_rubles = replenishment_amount / 100;
+            final_score = replenishment_amount + starting_score + bonus_rubles;
+            System.out.println("Клиент пополнил счёт на " + replenishment_amount + " рублей - бонус равен " + bonus_rubles + " рублям ,итоговая сумма на счету клиента - " + final_score);
 
         }
 
